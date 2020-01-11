4182cda9f37127ffc2e611bee86d5f4a226982cc
Peter Agrea <pagrea@nimr-mmrc.org>
2020-01-11 17:00:35 +0300

    Updated to fix some errors

---
+++ resources/views/partials/cdStatusBoxes.blade.php
@@ -0,0 +1,19 @@
+
+   <!-- ./col -->
+   <div class="col-lg-4 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-purple">
+      <div class="inner">
+        <h3> {{$pendingCDApproval}} </h3>
+
+        <p>Pending Center Director Approval </p>
+      </div>
+      <div class="icon">
+        <i class="fa fa-bar-chart"></i>
+      </div>
+      <a href="/hodsLeaveApprovalRequest" class="small-box-footer">
+        View them <i class="fa fa-arrow-circle-right"></i>
+      </a> 
+    </div>
+  </div>
+  <!-- ./col -->
